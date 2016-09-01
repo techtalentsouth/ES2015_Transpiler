520b3a5462228453c9682106ec1a70df6784665c
Shane Barringer <shane.barringer@outlook.com>
2016-09-01 17:55:13 -0400

    updates addToWindow function to use rest parameters

--- app/index.js
+++ app/index.js
@@ -1,7 +1,17 @@
-function addToWindow(obj){
-  document.getElementById('root')
-  .innerHTML = `<h3> ${obj} </h3>`
-  console.log(obj);
+function addToWindow(obj, ...args){
+  const output = document.getElementById('root')
+  console.log(args);
+  if (!args.length){
+    output.innerHTML = `<h3> ${obj} </h3>`
+  }else {
+    args.forEach((arg) => {
+      console.log(arg);
+      let item = document.createElement("h3")
+      item.innerHTML = ` ${arg}, `
+      output.appendChild(item)
+    })
+
+  }
 }
 
-addToWindow('hello world!!!!')
+addToWindow('hello world')
